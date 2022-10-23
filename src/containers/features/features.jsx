3328adcd2118c04219c67630b5f88f9a78ed607a
tamimhasan19702 <tamimhasancu@gmail.com>
2022-10-24 02:08:43 +0600

    features css updates

--- src/containers/features/features.jsx
+++ src/containers/features/features.jsx
@@ -33,7 +33,7 @@ const Features = () => {
     </div>
     <div className="gpt3-features-container">
      {featuresData.map((item,index) => (
-     <Feature />      
+     <Feature title={item.title} text={item.text} key={item.title + index}/>      
      ))}
     </div>
     </div>
