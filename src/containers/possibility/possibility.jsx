ca02ba8f353b996379b9f98c42aa206b39bdb6bd
tamimhasan19702 <tamimhasancu@gmail.com>
2022-10-24 19:01:57 +0600

    possibility section update

--- src/containers/possibility/possibility.jsx
+++ src/containers/possibility/possibility.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './possibility.css';
+import possibilityImg from '../../assets/possibility.png';
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className="gpt3-possibility section-padding" id='possibility'>
+    <div className="gpt3-possibility-image">
+      <img src={possibilityImg} alt="possibilityImg" />
+    </div>
+    <div className="gpt3-possibility-content">
+      <h4>Request Early Access to Get Started</h4>
+      <h1 className=''></h1>
+
+    </div>
+    </div>
   )
 }
 
