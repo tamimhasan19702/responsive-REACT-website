35ecf836acbedb5653b9b5ea641a3e8a9dc7e7f7
tamimhasan19702 <tamimhasancu@gmail.com>
2022-10-25 19:53:35 +0600

    article jsx created

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -7,6 +7,13 @@ const Article = ({imgUrl,date,title}) => {
       <div className="gpt3-blog-container-article-image">
         <img src={imgUrl} alt="blog" />
       </div>
+      <div className="gpt3-blog-container-article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
   )
 }
