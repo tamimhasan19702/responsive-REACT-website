dd6f69b2c08878716512974834a2d679c4685d59
tamimhasan19702 <tamimhasancu@gmail.com>
2022-10-25 18:53:13 +0600

    cta section updates

--- src/components/cta/cta.jsx
+++ src/components/cta/cta.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="gpt3-cta">
+      <div className="gpt3-cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3-cta-btn">
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
