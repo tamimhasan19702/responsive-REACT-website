d0ffc3af919a2a696578acdea03c22098d1654f6
tamimhasan19702 <tamimhasancu@gmail.com>
2022-10-25 19:01:04 +0600

    blog section jsx update

--- src/containers/blog/blog.jsx
+++ src/containers/blog/blog.jsx
@@ -3,7 +3,20 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+   <div className="gpt3-blog section-padding">
+    <div className="gpt3-blog-heading">
+      <h1 className='gradient-text'>A lot is happening, 
+We are blogging about it.</h1>
+    </div>
+    <div className="gpt3-blog-container">
+      <div className="gpt3-blog-container-groupA">
+        
+      </div>
+      <div className="gpt3-blog-container-groupB">
+
+      </div>
+    </div>
+   </div>
   )
 }
 
